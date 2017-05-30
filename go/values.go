04b33ccd4d0868337b70320b7387e0b873c5cc25
yeskunall <yeskunall96@gmail.com>
2017-05-30 12:42:08 -0400

    :hamster: Testing Go.

---
+++ go/values.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go " + "lang " + "handles string concat better than C!")
+	fmt.Println("semicolons are not compulsory, but inferred.")
+}
