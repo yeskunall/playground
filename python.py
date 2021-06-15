dd14b3d502ce06d1d6b1dcbbcf249570f5ca3f86
Kunall Banerjee <hey@kimchiii.space>
2021-06-15 04:17:12 -0400

    modify `python.py`
    Nothing to see here LMAO. Just testing my new PAT from GitHub.

--- python.py
+++ python.py
@@ -2,8 +2,19 @@
 
 import typer
 
-def main(name: str):
+app = typer.Typer()
+
+@app.command()
+def hello(name: str):
     typer.echo(f"Hello {name}")
 
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        typer.echo(f"Bye {name}!")
+
+
 if __name__ == "__main__":
-    typer.run(main)
+    app()
